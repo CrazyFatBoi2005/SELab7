aadcac393ec6292bfb391ba99d2c0111d66d3b0b
crazysadboy <54902146+CrazyFatBoi2005@users.noreply.github.com>
2025-11-11 01:40:45 +0500

    complete

--- src/main/java/com/uni/lab7/Lab7Application.java
+++ src/main/java/com/uni/lab7/Lab7Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lab7Application {
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Lab7Application.class, args);
     }
 
